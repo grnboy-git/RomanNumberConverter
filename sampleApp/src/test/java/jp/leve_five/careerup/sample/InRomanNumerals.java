f53b9bd87ea756204324add2bb754a77ebc7bc2b
andoumikihiro <andoumikihiro@192.168.10.15>
2014-03-10 14:04:19 +0900

    メソッド名の変更

--- sampleApp/src/test/java/jp/leve_five/careerup/sample/InRomanNumerals.java
+++ sampleApp/src/test/java/jp/leve_five/careerup/sample/InRomanNumerals.java
@@ -3,11 +3,11 @@ package jp.leve_five.careerup.sample;
 public class InRomanNumerals {
     private char element;
     private char previousElement = ' ';
-    
+
     public int inOut(String roman) {
         int Arabic = 0;
-        
-        Arabic = romanChangeArabic(Arabic, roman);
+
+        Arabic = changeArabicToRoman(Arabic, roman);
         if (Arabic < 1 || Arabic > 3999) {
             Arabic = 0;
             System.out.println("正しいローマ数字の記法ではありません");
@@ -15,25 +15,24 @@ public class InRomanNumerals {
         return Arabic;
     }
 
-    int romanChangeArabic(int Arabic, String roman) {
-        char[] arraySmallRoman = romanChangeArray(roman);
-        for (int i = arraySmallRoman.length - 1; i >= 0; i--) {
-            element = arraySmallRoman[i];
+    int changeArabicToRoman(int Arabic, String roman) {
+        char[] arrayRomanLowerCase = makeArrayOfRoman(roman);
+        for (int i = arrayRomanLowerCase.length - 1; i >= 0; i--) {
+            element = arrayRomanLowerCase[i];
             Arabic = checkCharAndCalc(Arabic, element, previousElement);
             previousElement = element;
         }
         return Arabic;
     }
-    
-    
-    char[] romanChangeArray(String roman) {
-        String smallRoman = roman.toLowerCase();
-        char[] arraySmallRoman = smallRoman.toCharArray();
-        return arraySmallRoman;
+
+    char[] makeArrayOfRoman(String roman) {
+        String romanToLowerCase = roman.toLowerCase();
+        char[] arrayRomanLowerCase = romanToLowerCase.toCharArray();
+        return arrayRomanLowerCase;
     }
 
     int checkCharAndCalc(int arabic, char element, char previousElement) {
-        switch(element){
+        switch (element) {
         case 'i':
             arabic = iCheck(arabic, previousElement);
             break;
@@ -56,75 +55,76 @@ public class InRomanNumerals {
             arabic = mCheck(arabic);
             break;
         default:
-            arabic -= 4000;
+            arabic = -4000;
         }
         return arabic;
     }
-    
-    int iCheck(int arabic, char previousElement){
+
+    int iCheck(int arabic, char previousElement) {
         if (arabic <= 2) {
             arabic += 1;
-        }else if ('v' == previousElement || 'x' == previousElement) {
+        } else if ('v' == previousElement || 'x' == previousElement) {
             arabic -= 1;
-        }else{
-            arabic -= 4000;
+        } else {
+            arabic = -4000;
         }
         return arabic;
     }
-    
-    int vCheck(int arabic){
-            if (arabic <= 3) {
-                arabic += 5;
-            }else{
-                arabic -= 4000;
-            }
-            return arabic;
+
+    int vCheck(int arabic) {
+        if (arabic <= 3) {
+            arabic += 5;
+        } else {
+            arabic = -4000;
+        }
+        return arabic;
     }
-    
-    int xCheck(int arabic, char previousElement){
+
+    int xCheck(int arabic, char previousElement) {
         if (arabic <= 29) {
             arabic += 10;
-        }else if ('l' == previousElement || 'c' == previousElement) {
+        } else if ('l' == previousElement || 'c' == previousElement) {
             arabic -= 10;
-        }else{
-            arabic -= 4000;
+        } else {
+            arabic = -4000;
         }
         return arabic;
     }
-    
-    int lCheck(int arabic){
-        if(arabic <= 39) {
+
+    int lCheck(int arabic) {
+        if (arabic <= 39) {
             arabic += 50;
-        }else{
-            arabic -= 4000;
+        } else {
+            arabic = -4000;
         }
         return arabic;
     }
-    
-    int cCheck(int arabic, char previousElement){
-        if(arabic <= 299) {
+
+    int cCheck(int arabic, char previousElement) {
+        if (arabic <= 299) {
             arabic += 100;
-        }else if ('d' == previousElement || 'm' == previousElement) {
+        } else if ('d' == previousElement || 'm' == previousElement) {
             arabic -= 100;
-        }else{
-            arabic -= 4000;
+        } else {
+            arabic = -4000;
         }
         return arabic;
     }
-    
-    int dCheck(int arabic){
-        if(arabic <= 399) {
+
+    int dCheck(int arabic) {
+        if (arabic <= 399) {
             arabic += 500;
-        }else{
-            arabic -= 4000;
+        } else {
+            arabic = -4000;
         }
         return arabic;
     }
-    int mCheck(int arabic){
-        if(arabic <= 2999) {
+
+    int mCheck(int arabic) {
+        if (arabic <= 2999) {
             arabic += 1000;
-        }else{
-            arabic -= 4000;
+        } else {
+            arabic = -4000;
         }
         return arabic;
     }
